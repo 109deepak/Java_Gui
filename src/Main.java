4353e7551095cd97aff15a1421405bc7771d0052
109deepak <drimal@students.federation.edu.au>
2019-06-06 14:50:43 +0300

    Variable declaration

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,34 @@ import java.util.List;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JFrame;
 import javax.swing.table.DefaultTableModel;
 
 
-public class Main extends JFrame implements ActionListener {
+public class Main extends JFrame implements  {
 
     List<Details> list = new ArrayList<Details>();
     DefaultTableModel model;
     JScrollPane scrollpane;
     Object[][] data;
+
+    // Variables declaration - do not modify
+    JTextField JTName,jThome,jToffice;
+    JButton jButton1, jButton2;
+    JCheckBox jCHome, jCOffice;
+
+    JLabel jLaddress, jLaname, jLhome, jLoffice;
+    JPanel jPanel1;
+    JScrollPane jScrollPane1;
+    JTextArea jTextArea1; // Variables declaration - do not modify
+    JTextField JTName,jThome,jToffice;
+    JButton jButton1, jButton2;
+    JCheckBox jCHome, jCOffice;
+
+    JLabel jLaddress, jLaname, jLhome, jLoffice;
+    JPanel jPanel1;
+    JScrollPane jScrollPane1;
+    JTextArea jTextArea1;
     public NewJFrame() {
         initComponents();
 
