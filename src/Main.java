9842953022fd049c84956d2e814c50ff4bd0bfd9
109deepak <drimal@students.federation.edu.au>
2019-06-06 12:30:37 +0300

    List to hold items

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
 import java.awt.event.ActionListener;
+import  javax.swing.*;
+
 
 public class Main extends JFrame implements ActionListener {
 
     //Declare variables to be used
-    Label  PizzaTittle, nameLabel,  addressLabel, contactLabel;
+    JLabel PizzaTittle, nameLabel, addressLabel, contactLabel;
     JTextField idField, nameField, addressField, contactField;
     JButton save, clear;
+    //List to hold the details
+    List<Details> list = new ArrayList<Details>();
+
+    class Details {
+        String Name;
+        String OfficeAddress;
+        String HomeAddress;
 
 
+        Details(String name, String Haddress, String Oaddress) {
+            this.Name = id;
+            this.OfficeAddress = name;
+            this.HomeAddress = Oaddress;
+
+
+        }
+    }
 }
