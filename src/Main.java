e8822efef790fef31933f75e8bc9616ae7a044c2
109deepak <drimal@students.federation.edu.au>
2019-06-06 11:49:41 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ import java.awt.event.ActionListener;
 
 public class Main extends JFrame implements ActionListener {
 
-    pu
+    //Declare variables to be used
+    Label  PizzaTittle, nameLabel,  addressLabel, contactLabel;
+    JTextField idField, nameField, addressField, contactField;
+    JButton save, clear;
 
 
 }
